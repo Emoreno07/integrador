4c00cf2c1793a9bf602a3c14391eb3982cd9711e
Emoreno07 <Emoreno22.0712@gmail.com>
2024-12-11 08:14:52 -0300

    removed fe from back branch

--- smartCityFE/src/components/footer/footer.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './footer.module.css'
-export default function Footer(){
-    return (
-        <footer className={`flex-container flex-center ${styles['footer']}`}>
-            <h1 className={`${styles['h1']}`}>SMARTCITY</h1>
-        </footer>
-    )
-}
